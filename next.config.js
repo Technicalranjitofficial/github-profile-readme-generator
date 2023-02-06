cc1c6f814f5d182c2f07a6ce6d735241a2c893c2
technicalranjitofficial <dranjitkumar16@gmail.com>
2023-02-06 20:43:37 +0530

    some bugs fixed

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   images:{
     domains:['github-readme-stats.vercel.app','github-readme-streak-stats.herokuapp.com','github-profile-trophy.vercel.app','user-images.githubusercontent.com','raw.githubusercontent.com']
+  },
+  env:{
+    GOOGLE_ANALYTICS: process.env.GOOGLE_ANALYTICS,
+    
+    
   }
 }
 
